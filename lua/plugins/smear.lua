16b3705dcefe084b99ede4a5d970ae173340af1c
kba0297 <kobebayliss1@gmail.com>
2026-05-07 15:52:34 +1200

    style: updating cursor colour

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,4 +1,14 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+
+  opts = {
+    cursor_color = "#957fb8",
+
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    distance_stop_animating = 0.5,
+
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+  },
 }
