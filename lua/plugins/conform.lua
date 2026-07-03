38e98905e4a5d4ea9b3c3f736fb5f5bb8d62bfbc
Kobe Bayliss <kobebayliss1@gmail.com>
2026-07-03 12:33:30 +1200

    feat: adding mason

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,19 @@
 return {
   "stevearc/conform.nvim",
+  event = { "BufWritePre" },
   opts = {
     format_on_save = {
       timeout_ms = 1000,
       lsp_fallback = true,
     },
-
     formatters_by_ft = {
       cpp = { "clang_format" },
       c = { "clang_format" },
       python = { "black" },
-
       javascript = { "prettier" },
       javascriptreact = { "prettier" },
       typescript = { "prettier" },
-      typescriptreact = { "prettier" }, 
+      typescriptreact = { "prettier" },
     },
   },
 }
